d03217115ee0e9504136596b8146b15b4b6a03c0
Soren Malling <soren@websupply.dk>
2026-05-05 20:22:41 +0200

    TASK: Ensure file path is returned before checking

--- Classes/Resource/Handler/ImageBuilderResource.php
+++ Classes/Resource/Handler/ImageBuilderResource.php
@@ -97,7 +97,7 @@ class ImageBuilderResource implements RemoteResourceInterface
         $gifBuilder = GeneralUtility::makeInstance(GifBuilder::class);
         $gifBuilder->start($fileArray, []);
         $theImage = $gifBuilder->gifBuild()?->getFullPath();
-        if (file_exists($theImage)) {
+        if ($theimage && file_exists($theImage)) {
             $content = file_get_contents($theImage);
             unlink($theImage);
         }
